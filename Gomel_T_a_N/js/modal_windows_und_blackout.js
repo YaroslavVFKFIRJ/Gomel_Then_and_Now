989c86d7076aaa307d5eb6d5dca574f3822fa074
YaroslavVFKFIRJ <150508580+YaroslavVFKFIRJ@users.noreply.github.com>
2024-10-29 13:50:06 +0300

    Update modal_windows_und_blackout.js

--- Gomel_T_a_N/js/modal_windows_und_blackout.js
+++ Gomel_T_a_N/js/modal_windows_und_blackout.js
@@ -3,7 +3,7 @@ document.querySelectorAll(".clickable").forEach(Element => Element.onclick = () 
     document.querySelector("#cross_blackout").classList.toggle("active");
     document.querySelector("#H5_content").innerHTML = Element.getAttribute("data-place");
     document.querySelector(`#${Element.getAttribute("data-place").split(" ").join("")}`).classList.add("active"); 
-    document.querySelector("#NEW_photography").setAttribute(`src`, `Gomel_T_a_N/media/img/NEW/${Element.getAttribute("data-place")}_new.jpg`);
+    document.querySelector("#NEW_photography").setAttribute(`src`, ``);
     document.querySelector("#OLD_photography").setAttribute(`src`, `Gomel_T_a_N/media/img/OLD/${Element.getAttribute("data-place")}_old.jpg`);
     document.querySelector("#body").style.overflow = "hidden";
 });
